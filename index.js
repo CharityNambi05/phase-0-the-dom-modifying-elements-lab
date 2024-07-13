89551216a8b92a26d4be240098985498783e823a
CharityNambi05 <charity.nambi@icloud.com>
2024-07-13 22:19:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const main = document.querySelector('#main');
+if (main) {
+  main.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+const yourName = 'John Doe'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+
+document.body.appendChild(newHeader);
+
+
